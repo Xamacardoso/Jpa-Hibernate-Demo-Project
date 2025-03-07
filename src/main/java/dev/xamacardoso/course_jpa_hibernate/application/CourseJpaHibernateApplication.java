85f178ae48f505f1cd6638eeaee257c7133a5d2f
Xamacardoso <xcardoso1997@gmail.com>
2025-03-06 21:49:55 -0300

    Feat (Test Profile): Added H2 database, test profile and JPA configs

--- src/main/java/dev/xamacardoso/course_jpa_hibernate/application/CourseJpaHibernateApplication.java
+++ src/main/java/dev/xamacardoso/course_jpa_hibernate/application/CourseJpaHibernateApplication.java
@@ -2,10 +2,14 @@ package dev.xamacardoso.course_jpa_hibernate.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"dev.xamacardoso.course_jpa_hibernate"})
+@EntityScan(basePackages = {"dev.xamacardoso.course_jpa_hibernate"})
+@EnableJpaRepositories(basePackages = {"dev.xamacardoso.course_jpa_hibernate"})
 public class CourseJpaHibernateApplication {
 
 	public static void main(String[] args) {
