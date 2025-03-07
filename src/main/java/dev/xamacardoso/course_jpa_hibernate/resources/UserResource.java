835e7b91032c82948e86b481d2e1215b5a381bdb
Xamacardoso <xcardoso1997@gmail.com>
2025-03-06 22:39:22 -0300

    Feat (User): Added service layer, component registration, findAll  and findById implementation

--- src/main/java/dev/xamacardoso/course_jpa_hibernate/resources/UserResource.java
+++ src/main/java/dev/xamacardoso/course_jpa_hibernate/resources/UserResource.java
@@ -1,21 +1,35 @@
 package dev.xamacardoso.course_jpa_hibernate.resources;
 
 import dev.xamacardoso.course_jpa_hibernate.entities.User;
+import dev.xamacardoso.course_jpa_hibernate.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     // ResponseEntity defines which entity will be retrieved from entities
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User user = new User(1L, "Jonas", "jonas@gmail", "+559684475525", "kleinianrs");
+    public ResponseEntity<List<User>> findAll(){
+        List<User> users = userService.findAll();
 
-        // Returns a entity with status code OK
+        return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User user = userService.findById(id);
+
         return ResponseEntity.ok().body(user);
     }
 }
